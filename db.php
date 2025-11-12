4e3ff93c49f042c5171b999710b9b21846e1f661
expressoption <expressoptionofficial@gmail.com>
2025-11-12 10:14:13 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,19 @@
-<?php
-require 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+use MongoDB\Client;
+use MongoDB\BSON\ObjectId;
 
-$mongoUri = getenv('MONGO_URI');
 
 try {
-    $client = new MongoDB\Client($mongoUri);
-    $db = $client->selectDatabase('mydatabase'); // name your DB
-    $collection = $db->selectCollection('users');
+$client = new Client("mongodb://127.0.0.1:27017");
+$db = $client->selectDatabase('user_auth');
+$users = $db->selectCollection('users');
 } catch (Exception $e) {
-    die("Database connection failed: " . $e->getMessage());
+die('Could not connect to MongoDB: ' . $e->getMessage());
 }
-?>
+
+function toMongoId($id) {
+if (is_string($id) && preg_match('/^[0-9a-f]{24}$/i', $id)) {
+return new ObjectId($id);
+}
+return $id;
+}
